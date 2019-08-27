2348d8ad2731d91970b8252b458b4e42faaf06f5
Mauricio Oleaga <maoa.develoment@gmail.com>
2019-08-27 00:14:10 -0400

    importacion de files y conectar con el port

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+                        //este "app" podria cambiar OJO 
+server.listen(port);
+console.log('server listen on:', port);
