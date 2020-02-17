f224e8afa96fd8777e40d227953ee68a7ab133d0
Mauricio Oleaga <maoa.develoment@gmail.com>
2020-02-17 12:34:47 -0300

    Se acomoda el objecto error en el profilejs del router del backend y profilejs del actions

--- ui/src/actions/profile.js
+++ ui/src/actions/profile.js
@@ -10,10 +10,10 @@ export const getProfile = () => async dispatch => {
             type: GET_PROFILE,
             payload: res.data
         });
-    } catch (e) {
+    } catch (error) {
         dispatch({
             type: PROFILE_ERROR,
-            payload: {msg: e.response.statusText, status: e.response.status}
+            payload: {msg: error.response.statusText, status: error.response.status}
             // en este payload se va a pasar un mensaje al usuario "msg" y aparte el "status" con el "e.response.status" va a mandar un 400 o algo parecido indicando el error
         })
     }
