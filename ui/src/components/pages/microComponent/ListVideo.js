a917c62035f2301869870c903c501733600a2525
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-03-25 11:46:36 -0300

    The following files are deleted; router.txt, index.txt, index.tets.js, Youtube.js, Google.js, App.test.js, RoutynesType.test.js, EditProfile.txt, cardProfile.scss, Barra.js, VideoDetail.js, VideoItem.css, VideoItem.js, VideoList.js y musculoReducer.test.js because they were being files that were extra in the project. Descriptive comments are removed from the following scss file;  config.scss, form.scss, index.scss, modal.scss, spinner.scss y utilities.scss leaving only the essential content. New images have been added in the api

--- ui/src/components/pages/microComponent/ListVideo.js
+++ ui/src/components/pages/microComponent/ListVideo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import '../../videoTools/VideoItem.css';
+
 
 const ListVideo = ({ history, video: { modo, profesor, duracion, imagen, idioma, _id } }) => {
     // "video" viene como props del otro component VideoList.. gracias a el podemos tener acceso a las propiedades y lo mismo con "history"
--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -1,3 +1,4 @@
+// For tests
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
